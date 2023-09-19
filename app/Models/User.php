a42a6a01b6097c7d16591504edaf7a5481da725b
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2023-09-19 10:07:43 +0700

    chore: setup db and templating design dashboard

---
+++ app/Models/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $table = 'users';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    const CREATED_AT = 'created_at';
+    const UPDATED_AT = 'updated_at';
+}
